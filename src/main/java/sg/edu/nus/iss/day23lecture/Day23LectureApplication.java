241a638ebf946b8eb8c920c962ed3a6f404dcaf7
sunilsingh15 <sunilsingh.1996@hotmail.com>
2023-06-22 13:53:48 +0800

    initial commit

---
+++ src/main/java/sg/edu/nus/iss/day23lecture/Day23LectureApplication.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.iss.day23lecture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day23LectureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Day23LectureApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sg/edu/nus/iss/day23lecture/Day23LectureApplicationTests.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.iss.day23lecture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Day23LectureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
